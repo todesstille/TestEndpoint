e430029db771e81647507260708a9a2fb4e88bf2
Dmitry Redkin <tanatonaut@gmail.com>
2024-08-11 22:37:03 +0300

    await when delete

--- src/features/posts/controllers/deletePostController.ts
+++ src/features/posts/controllers/deletePostController.ts
@@ -2,7 +2,7 @@ import {Response, Request} from 'express';
 import { db } from '../../../app';
 
 export const deletePostController = async (req: Request, res: Response) => {
-    const blog = db.findPosts(req.params.id);
+    const blog = await db.findPosts(req.params.id);
     if (blog == undefined) {
         res.send(404);
     } else {
